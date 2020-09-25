9ba3a922f16d2beba81a154f2063cf15db7e9a08
Said Maadan <me@saidmaadan.com>
2020-09-24 23:08:59 -0500

    Project setup

---
+++ config/webpack/environment.js
@@ -0,0 +1,9 @@
+const webpack = require('webpack')
+environment.plugins.append('Provide',
+    new webpack.ProvidePlugin({
+        $: 'jquery/src/jquery',
+        jQuery: 'jquery/src/jquery'
+    })
+)
+
+module.exports = environment
